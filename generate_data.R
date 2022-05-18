3ca5688f98b18bd9487c06f7f8d93f0bae8c5524
ali <hashea@mcmaster.ca>
2022-05-17 21:46:18 -0400

    s2022 test 1 upload

---
+++ generate_data.R
@@ -0,0 +1,6 @@
+set.seed(20220518)
+conData <- data.frame(adolescence=abs(round(rnorm(25,2,1))), teens=abs(round(rnorm(25,2.2,2))))
+
+set.seed(20220518)
+loudnessData <- data.frame(bold=round(rnorm(12,mean=64,sd=3)), italics=round(rnorm(12,mean=60,sd=3)))
+
