f5a6371f955a47166b714f9ca2d434824e6d7a74
ali <hashea@mcmaster.ca>
2022-10-03 18:56:12 -0400

    updated for test fall 2022

--- generate_data.R
+++ generate_data.R
@@ -1,6 +1,5 @@
-set.seed(20220525)
-conData <- data.frame(adolescence=abs(round(rnorm(25,2,1))), teens=abs(round(rnorm(25,2.2,2))))
+set.seed(20220907)
+conscData <- data.frame(younger=abs(round(rnorm(25,2,1))), older=abs(round(rnorm(25,2.2,2))))
 
 set.seed(20220518)
-loudnessData <- data.frame(bold=round(rnorm(12,mean=64,sd=3)), italics=round(rnorm(12,mean=60,sd=3)))
-
+loudData <- data.frame(red=round(rnorm(12,mean=64,sd=3)), blue=round(rnorm(12,mean=60,sd=3)))
