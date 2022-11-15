b5468bd12f511f1528d1751eaabb20d9cd98bfcd
ali <hashea@mcmaster.ca>
2022-11-15 07:02:30 -0500

    removed old stuff

--- generate_data.R
+++
@@ -1,5 +0,0 @@
-set.seed(20220907)
-conscData <- data.frame(younger=abs(round(rnorm(25,2,1))), older=abs(round(rnorm(25,2.2,2))))
-
-set.seed(20220518)
-loudData <- data.frame(red=round(rnorm(12,mean=64,sd=3)), blue=round(rnorm(12,mean=60,sd=3)))
--- generate_data_late.R
+++
@@ -1,5 +0,0 @@
-set.seed(20221019)
-conscData <- data.frame(younger=abs(round(rnorm(33,1.75,1.5))), older=abs(round(rnorm(33,2.55,2))))
-
-set.seed(20221019)
-loudData <- data.frame(red=round(rnorm(14,mean=64,sd=3)), blue=round(rnorm(14,mean=60,sd=3)))
